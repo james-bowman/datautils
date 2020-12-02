dc96b1c6f35942aa85f091b57e7eb0ba8c4cc311
james-bowman <james.edward.bowman@gmail.com>
2020-12-02 15:30:47 +0000

    added PrecisionAt and RPrecision methods

--- metrics.go
+++ metrics.go
@@ -9,49 +9,88 @@ import (
 	"gonum.org/v1/plot/plotter"
 )
 
-// PrecisionRecallCurve represents a precision recall curve for visualising and measuring the performance of a
-// classification or information retrieval model.  It can be used to evaluate how well the model predictions
-// can be ranked compared to a perfect ranking according to the ground truth labels.  This is usefull when
-// evaluating ranking based on relevancy for information retrieval or raw classification performance based on
-// predicted probability of class membership e.g. logistic regression predictions without using a threshold to
-// determine the class for the predicted probability.  To measure Precision@K using the precision recall curve
-// where K is the cut-off simply index Precision e.g. Precision[K] or to measure Precision@1 index Precision[1].
-// It is important to note that Precision[0] and Recall[0] indicate the precision and recall @ 0 and so will always
-// be 1 and 0 respectively.
-type PrecisionRecallCurve struct {
-	// Precision is a slice containing the ranked precision values at K for the predictions until all positive/
-	// relevant items were found according to corresponding the ground truth labels (recall==1)
-	Precision []float64
-
-	// Recall is a slice containing the ranked recall values at K for the predictions until all positive/
-	// relevant items were found according to corresponding the ground truth labels (recall==1)
-	Recall []float64
-
+type DiscountedCumulativeGain struct {
 	// Thresholds is a slice containing the ranked (sorted) predictions (probability/similarity scores) until
 	// all positive/relevant items were found according to corresponding the ground truth labels (recall==1)
 	Thresholds []float64
 }
 
-// NewPrecisionRecallCurve creates a new precision recall curve.  The precision recall curve visualises how well
-// the model's predictions (or similarity scores for information retrieval) can be ranked compared to a perfect
-// ranking according to the ground truth labels.  Both the supplied predictions and labels slices can be in any
-// order providing they are identical lengths and their order matches e.g. predictions[5] corresponds to the
-// ground truth labels[5].  As Precision Recall curves and average precision (summarising the curve as a single
-// metric/area under the curve) represent a binary class/relevance measure we assume that any label value greater
-// than 0 represents a positive/relative observation (and 0 label values represent a negative/non-relevant
-// observation).
-func NewPrecisionRecallCurve(predictions, labels []float64) PrecisionRecallCurve {
+/*
+func NewDiscountedCumulativeGain(predictions, labels []float64) DiscountedCumulativeGain {
 	if len(predictions) != len(labels) {
 		panic("Prediction/Label length mismatch")
 	}
 
 	thresholds := make([]float64, len(predictions))
+	ind := make([]int, len(predictions))
+
+	cg := make([]float64, len(predictions))
+	dcg := make([]float64, len(predictions))
+	perfectdcg := make([]float64, len(predictions))
+
 	recall := make([]float64, len(predictions))
 	precision := make([]float64, len(predictions))
-	ind := make([]int, len(predictions))
+
+
+	maxRelevance := floats.Max(labels)
+	if maxRelevance == 0 {
+		return DiscountedCumulativeGain{
+			Thresholds: thresholds[:0],
+		}
+	}
+
+	// rank predictions/similarities
+	copy(thresholds, predictions)
+	floats.Argsort(thresholds, ind)
+
+	dcg[0] = labels[ind[len(ind)-1]]
+	k := 1
+
+	for i := len(ind) - 2; i >= 0; i-- {
+		dcg[k] = dcg[k-1] + labels[ind[i]]/math.Log2(k)
+		k++
+	}
+
+	for i := maxRelevance; i >= 0; i-- {
+		count := floats.Count(func(x float64) bool {
+			if x == i {
+				return true
+			}
+			return false
+		}, labels)
+
+	}
+
+	perfectdcg[0] = maxRelevance
+
+	for i := 1; i < len(perfectdcg); i++ {
+
+	}
+
+
+
+		// assume that any label value over 0 is positive/relevant.  Average precision works on a binary label
+		// but in some cases we may use non-binary/multi-class labels e.g. for degrees of relevancy in information
+		// retrieval
+		if labels[ind[i]] > 0 {
+			hits++
+		}
+		recall[k] = float64(hits) / float64(positives)
+		precision[k] = float64(hits) / float64(k+1)
+		if recall[k] == 1 {
+			break
+		}
+		k++
+	}
+
+
+
+
+
 
 	// count total positive/relevant observations from ground truth
 	// as floats.Norm() does not work for zero norm we will use floats.Count() instead
+
 	positives := floats.Count(func(x float64) bool {
 		if x > 0 {
 			return true
@@ -104,6 +143,101 @@ func NewPrecisionRecallCurve(predictions, labels []float64) PrecisionRecallCurve
 		Thresholds: thresholds[len(thresholds)-k-1:],
 	}
 }
+*/
+
+// PrecisionRecallCurve represents a precision recall curve for visualising and measuring the performance of a
+// classification or information retrieval model.  It can be used to evaluate how well the model predictions
+// can be ranked compared to a perfect ranking according to the ground truth labels.  This is usefull when
+// evaluating ranking based on relevancy for information retrieval or raw classification performance based on
+// predicted probability of class membership e.g. logistic regression predictions without using a threshold to
+// determine the class for the predicted probability.
+// It is important to note that Precision[0] and Recall[0] indicate the precision and recall @ 0 and so will always
+// be 1 and 0 respectively.
+type PrecisionRecallCurve struct {
+	// Precision is a slice containing the ranked precision values at K for the predictions until all positive/
+	// relevant items were found according to corresponding the ground truth labels (recall==1)
+	Precision []float64
+
+	// Recall is a slice containing the ranked recall values at K for the predictions until all positive/
+	// relevant items were found according to corresponding the ground truth labels (recall==1)
+	Recall []float64
+
+	// Thresholds is a slice containing the ranked (sorted) predictions (probability/similarity scores) until
+	// all positive/relevant items were found according to corresponding the ground truth labels (recall==1)
+	Thresholds []float64
+
+	positives int
+}
+
+// NewPrecisionRecallCurve creates a new precision recall curve.  The precision recall curve visualises how well
+// the model's predictions (or similarity scores for information retrieval) can be ranked compared to a perfect
+// ranking according to the ground truth labels.  Both the supplied predictions and labels slices can be in any
+// order providing they are identical lengths and their order matches e.g. predictions[5] corresponds to the
+// ground truth labels[5].  As Precision Recall curves and average precision (summarising the curve as a single
+// metric/area under the curve) represent a binary class/relevance measure we assume that any label value greater
+// than 0 represents a positive/relative observation (and 0 label values represent a negative/non-relevant
+// observation).
+func NewPrecisionRecallCurve(predictions, labels []float64) PrecisionRecallCurve {
+	if len(predictions) != len(labels) {
+		panic("Prediction/Label length mismatch")
+	}
+
+	thresholds := make([]float64, len(predictions))
+	recall := make([]float64, len(predictions))
+	precision := make([]float64, len(predictions))
+	ind := make([]int, len(predictions))
+
+	// count total positive/relevant observations from ground truth
+	// as floats.Norm() does not work for zero norm we will use floats.Count() instead
+	positives := floats.Count(func(x float64) bool {
+		return (x > 0)
+	}, labels)
+
+	if positives == 0 {
+		return PrecisionRecallCurve{
+			Precision:  append(precision[:0], 1),
+			Recall:     append(recall[:0], 0),
+			Thresholds: thresholds[:0],
+			positives:  positives,
+		}
+	}
+
+	// rank predictions/similarities
+	copy(thresholds, predictions)
+	floats.Argsort(thresholds, ind)
+
+	var hits int
+	var k int
+
+	for i := len(ind) - 1; i >= 0; i-- {
+		// assume that any label value over 0 is positive/relevant.  Average precision works on a binary label
+		// but in some cases we may use non-binary/multi-class labels e.g. for degrees of relevancy in information
+		// retrieval
+		if labels[ind[i]] > 0 {
+			hits++
+		}
+		recall[k] = float64(hits) / float64(positives)
+		precision[k] = float64(hits) / float64(k+1)
+		if recall[k] == 1 {
+			break
+		}
+		k++
+	}
+	// truncate precision and recall to where the last relevant/positive item was ranked (recall==1)
+	precision = precision[:k+1]
+	recall = recall[:k+1]
+
+	// reverse order so highest similarity/probability is ranked higher/first
+	floats.Reverse(precision)
+	floats.Reverse(recall)
+
+	return PrecisionRecallCurve{
+		Precision:  append(precision, 1),
+		Recall:     append(recall, 0),
+		Thresholds: thresholds[len(thresholds)-k-1:],
+		positives:  positives,
+	}
+}
 
 // Plot renders the entire precision recall curve as a plot for visualisation.
 func (c PrecisionRecallCurve) Plot() *plot.Plot {
@@ -138,11 +272,58 @@ func (c PrecisionRecallCurve) Plot() *plot.Plot {
 // constructed with.  Average Precision represents the area under the curve of the precision recall curve
 // and is a method for summarising the curve in a single metric.
 func (c PrecisionRecallCurve) AveragePrecision() float64 {
+	//	var sum float64
+
 	var sum float64
 	for i := 0; i < len(c.Precision)-1; i++ {
 		sum += (c.Recall[i+1] - c.Recall[i]) * c.Precision[i]
 	}
 	return -sum
+}
+
+// AverageInterpolatedPrecision calculates the average interpolated precision based on the predictions and labels
+// the curve was constructed with.  Average Interpolated Precision represents the area under the curve of the precision
+// recall curve using interpolated precision for 11 fixed recall values {0.0, 0.1, 0.2, ... 1.0}.
+func (c PrecisionRecallCurve) AverageInterpolatedPrecision() float64 {
+	var sum float64
+	for i := 0; i <= 10; i++ {
+		sum += c.InterpolatedPrecisionAt(float64(i) / 10.0)
+	}
+	return sum / 11.0
+}
+
+// RPrecision returns the R-Precision.  The total number of relevant documents, R, is used as the cutoff
+// for calculation, and this varies from query to query. It counts the number of results ranked above the
+// cutoff that are relevant, r, and turns that into a relevancy fraction: r/R.
+func (c PrecisionRecallCurve) RPrecision() float64 {
+	return c.Precision[len(c.Precision)-1-c.positives]
+}
+
+// PrecisionAt calculates the Precision@k.  This represents the precision at a certain cut-off, k i.e.
+// if a search returns 10 (k=10) results what is the proportion of those 10 results that are relevant or
+// if we are only interested in the relevancy of the top ranked item (k=1) is that item relevant or not.
+func (c PrecisionRecallCurve) PrecisionAt(k int) float64 {
+	return c.Precision[len(c.Precision)-1-k]
+}
+
+// InterpolatedPrecisionAt calculates an interpolated Precision@r.  This can be used to calculate the precision for
+// a specific recall value that does not necessarily occur explicitly in the ranking.  It is calculated by taking the
+// maximum precision value over all recalls greater than r.
+func (c PrecisionRecallCurve) InterpolatedPrecisionAt(r float64) float64 {
+	// max precision [ recall > r]
+	var inds []int
+	var err error
+	if inds, err = floats.Find(inds, func(x float64) bool { return (x >= r) }, c.Recall, -1); err != nil {
+		panic("Failed to find items with recall higher " + err.Error())
+	}
+
+	var max float64
+	for _, v := range inds {
+		if c.Precision[v] > max {
+			max = c.Precision[v]
+		}
+	}
+	return max
 }
 
 type ConfusionMatrix struct {
--- metrics_test.go
+++ metrics_test.go
@@ -1,68 +1,77 @@
 package datautils_test
 
 import (
+	"math"
 	"testing"
 
 	"github.com/gonum/floats"
 	"github.com/james-bowman/datautils"
 )
 
-func TestPrecisionRecall(t *testing.T) {
+var datasets = []struct {
+	// ground truth relevance and predicted relevancy scores
+	labels []float64
+	probs  []float64
+}{
+	{
+		labels: []float64{0, 0, 1, 1},
+		probs:  []float64{0.1, 0.4, 0.35, 0.8},
+	},
+	{
+		labels: []float64{0, 0, 1, 1, 0},
+		probs:  []float64{0.1, 0.4, 0.35, 0.8, 0.85},
+	},
+	{
+		labels: []float64{1, 0, 0, 1, 1, 0},
+		probs:  []float64{0.02, 0.1, 0.4, 0.35, 0.8, 0.85},
+	},
+	{
+		labels: []float64{0, 0},
+		probs:  []float64{0.02, 0.1},
+	},
+	{
+		labels: []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+		probs:  []float64{0.001485745854553862, 0.0014863790364460178, 0.0014863790364460178, 0.0014854873139097426, 0.001485745854553862, 0.001485745854553862, 0.0014863790364460178, 0.0014863790364460178, 0.001485745854553862, 0.001485745854553862, 0.0014863646408943988, 0.0014857314651254725, 0.0014857314651254725, 0.0014857314651254725, 0.0014863646408943988, 0.0014857314651254725, 0.0014863646408943988},
+	},
+}
+
+func TestPrecisionRecallCurveCreation(t *testing.T) {
 	// Test the metric functions
 	tests := []struct {
-		// inputs
-		labels []float64
-		probs  []float64
 		// expected
 		precision  []float64
 		recall     []float64
 		thresholds []float64
-		ap         float64
 	}{
 		{
-			labels:     []float64{0, 0, 1, 1},
-			probs:      []float64{0.1, 0.4, 0.35, 0.8},
-			precision:  []float64{0.6666666666666666, 0.5, 1, 1},
+			precision:  []float64{2.0 / 3.0, 0.5, 1, 1},
 			recall:     []float64{1, 0.5, 0.5, 0},
 			thresholds: []float64{0.35, 0.4, 0.8},
-			ap:         0.8333333333333333,
 		},
 		{
-			labels:     []float64{0, 0, 1, 1, 0},
-			probs:      []float64{0.1, 0.4, 0.35, 0.8, 0.85},
-			precision:  []float64{0.5, 0.3333333333333333, 0.5, 0, 1},
+			precision:  []float64{0.5, 1.0 / 3.0, 0.5, 0, 1},
 			recall:     []float64{1, 0.5, 0.5, 0, 0},
 			thresholds: []float64{0.35, 0.4, 0.8, 0.85},
-			ap:         0.500000,
 		},
 		{
-			labels:     []float64{1, 0, 0, 1, 1, 0},
-			probs:      []float64{0.02, 0.1, 0.4, 0.35, 0.8, 0.85},
-			precision:  []float64{0.5, 0.4, 0.5, 0.3333333333333333, 0.5, 0, 1},
-			recall:     []float64{1, 0.6666666666666666, 0.6666666666666666, 0.3333333333333333, 0.3333333333333333, 0, 0},
+			precision:  []float64{0.5, 0.4, 0.5, 1.0 / 3.0, 0.5, 0, 1},
+			recall:     []float64{1, 2.0 / 3.0, 2.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0, 0, 0},
 			thresholds: []float64{0.02, 0.1, 0.35, 0.4, 0.8, 0.85},
-			ap:         0.500000,
 		},
 		{
-			labels:     []float64{0, 0},
-			probs:      []float64{0.02, 0.1},
 			precision:  []float64{1},
 			recall:     []float64{0},
 			thresholds: []float64{},
-			ap:         0.0,
 		},
 		{
-			labels:     []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			probs:      []float64{0.001485745854553862, 0.0014863790364460178, 0.0014863790364460178, 0.0014854873139097426, 0.001485745854553862, 0.001485745854553862, 0.0014863790364460178, 0.0014863790364460178, 0.001485745854553862, 0.001485745854553862, 0.0014863646408943988, 0.0014857314651254725, 0.0014857314651254725, 0.0014857314651254725, 0.0014863646408943988, 0.0014857314651254725, 0.0014863646408943988},
 			precision:  []float64{1},
 			recall:     []float64{0},
 			thresholds: []float64{},
-			ap:         0.0,
 		},
 	}
 
-	for _, test := range tests {
-		curve := datautils.NewPrecisionRecallCurve(test.probs, test.labels)
+	for i, test := range tests {
+		curve := datautils.NewPrecisionRecallCurve(datasets[i].probs, datasets[i].labels)
 		if !floats.Equal(test.precision, curve.Precision) {
 			t.Errorf("Expected precision: %v but received %v", test.precision, curve.Precision)
 		}
@@ -72,9 +81,120 @@ func TestPrecisionRecall(t *testing.T) {
 		if !floats.Equal(test.thresholds, curve.Thresholds) {
 			t.Errorf("Expected thresholds: %v but received %v", test.thresholds, curve.Thresholds)
 		}
+	}
+}
+
+func TestAveragePrecision(t *testing.T) {
+	// Test the metric functions
+	tests := []struct {
+		// expected
+		ap float64
+	}{
+		{ap: 0.8333333333333333},
+		{ap: 0.500000},
+		{ap: 0.500000},
+		{ap: 0.0},
+		{ap: 0.0},
+	}
+
+	for i, test := range tests {
+		curve := datautils.NewPrecisionRecallCurve(datasets[i].probs, datasets[i].labels)
 		ap := curve.AveragePrecision()
 		if ap != test.ap {
 			t.Errorf("Expected AP: %f but received %f", test.ap, ap)
+		}
+	}
+}
+
+func TestAverageInterpolatedPrecision(t *testing.T) {
+	// Test the metric functions
+	tests := []struct {
+		// expected
+		ap float64
+	}{
+		{ap: 0.848485},
+		{ap: 0.545455},
+		{ap: 0.545455},
+		{ap: 0.090909},
+		{ap: 0.090909},
+	}
+
+	for i, test := range tests {
+		curve := datautils.NewPrecisionRecallCurve(datasets[i].probs, datasets[i].labels)
+		ap := curve.AverageInterpolatedPrecision()
+
+		if math.Abs(ap-test.ap) > 0.000001 {
+			t.Errorf("Expected AInterpP: %.6f but received %.6f", test.ap, ap)
+		}
+	}
+}
+
+func TestPrecisionAtK(t *testing.T) {
+	// Test the metric functions
+	tests := []struct {
+		// expected
+		precisions []float64
+	}{
+		{precisions: []float64{1, 1, 0.5, 2.0 / 3.0}},
+		{precisions: []float64{1, 0, 0.5, 1.0 / 3.0, 0.5}},
+		{precisions: []float64{1, 0, 0.5, 1.0 / 3.0, 0.5, 0.4, 0.5}},
+		{precisions: []float64{1}},
+		{precisions: []float64{1}},
+	}
+
+	for i, test := range tests {
+		curve := datautils.NewPrecisionRecallCurve(datasets[i].probs, datasets[i].labels)
+		for k, v := range test.precisions {
+			pAtk := curve.PrecisionAt(k)
+			if pAtk != v {
+				t.Errorf("Test %d. Expected P@%d: %f but received %f", i, k, v, pAtk)
+			}
+		}
+	}
+}
+
+func TestRPrecision(t *testing.T) {
+	// Test the metric functions
+	tests := []struct {
+		// expected
+		rprecision float64
+	}{
+		{rprecision: 0.5},
+		{rprecision: 0.5},
+		{rprecision: 1.0 / 3.0},
+		{rprecision: 1},
+		{rprecision: 1},
+	}
+
+	for i, test := range tests {
+		curve := datautils.NewPrecisionRecallCurve(datasets[i].probs, datasets[i].labels)
+		RP := curve.RPrecision()
+		if RP != test.rprecision {
+			t.Errorf("Expected RPrecision: %f but received %f", test.rprecision, RP)
+		}
+	}
+}
+
+func TestInterpolatedPrecisionAtR(t *testing.T) {
+	// Test the metric functions
+	tests := []struct {
+		// expected
+		precisions []float64
+	}{
+		{precisions: []float64{1, 1, 1, 1, 1, 1, 2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0}},
+		{precisions: []float64{1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5}},
+		{precisions: []float64{1, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5}},
+		{precisions: []float64{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+		{precisions: []float64{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}},
+	}
+
+	for i, test := range tests {
+		curve := datautils.NewPrecisionRecallCurve(datasets[i].probs, datasets[i].labels)
+		for r, v := range test.precisions {
+			pAtr := curve.InterpolatedPrecisionAt(float64(r) / 10)
+			if pAtr != v {
+				t.Errorf("Test %d. Expected Interp. P@%.1f: %f but received %f", i, float64(r)/10, v, pAtr)
+			}
 		}
 	}
 }
